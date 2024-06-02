6b1b0971380ad494dd9d87135797d3aad29f92bf
siwaneden <142535063+siwaneden@users.noreply.github.com>
2024-06-02 14:20:33 -0300

    hmymym

--- cadastro/cadastro/settings.py
+++ cadastro/cadastro/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'cadastro.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Biblioteca',
+        'USER': 'root',
+        'PASSWORD': 'Burro2121?',
+        'HOST': 'localhost',  # Ou o endereço IP do servidor MySQL
+        'PORT': '3306',  # O porto padrão para MySQL
     }
 }
 
