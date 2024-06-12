db2b85acd0d9e47d211875b430e7b463b3599798
siwaneden <142535063+siwaneden@users.noreply.github.com>
2024-06-12 08:10:59 -0300

    Update settings.py

--- cadastro/cadastro/settings.py
+++ cadastro/cadastro/settings.py
@@ -127,3 +127,20 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        '': { 
+            'handlers': ['console'],
+            'level': 'INFO',
+            'propagate': True,
+        },
+    },
+}
